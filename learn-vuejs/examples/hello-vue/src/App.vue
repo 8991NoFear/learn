fe4ef0d8a48176820fd5a96170aa0204031dd1ae
BinhLD <binhld8504@co-well.vn>
2021-11-11 14:33:44 +0700

    Vue hello world with some input

---
+++ learn-vuejs/examples/hello-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Menu />
+    <input type="text" v-model="inputText">
+    {{ inputText }}
+    <input type="checkbox" v-model="inputCheckbox">Accept terms&service
+    <input type="radio" value="1" v-model="inputRadio">1
+    <input type="radio" value="2" v-model="inputRadio">2
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+  components: {
+    Menu // Menu: Menu
+  },
+  data() {
+    return {
+      inputText: "BinhLD",
+      inputCheckbox: true,
+      inputRadio: 2
+    }
+  }
+}
+</script>
+
+<style>
+</style>
