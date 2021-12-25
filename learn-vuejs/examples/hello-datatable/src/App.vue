3351aa155a77a1d72e16998c41c4e34cbf4c64a1
8991nofear <8991nofear@gmail.com>
2021-12-26 00:07:15 +0700

    init example project

---
+++ learn-vuejs/examples/hello-datatable/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="container-xl">
+    <div class="row pt-5">
+      <div class="col-md-10 offset-md-1">
+        <div class="card h-100">
+          <div class="card-body">
+            <h5>Vaccination sites by locations</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
