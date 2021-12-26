bd36608e64fc44097c36842756414a3769892471
8991nofear <8991nofear@gmail.com>
2021-12-26 23:49:27 +0700

    basic structure

---
+++ learn-vuejs/examples/hello-laravel-vue/resources/js/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    "hello-world": HelloWorld,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Pushster&display=swap');
+
+#app {
+  font-family: Pushster, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ learn-vuejs/examples/hello-laravel-vue/resources/js/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello laravel-vue</h1>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<style>
+</style>
