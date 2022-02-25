bda50feda3fefb37aba889315b27a805f728a660
8991nofear <8991nofear@gmail.com>
2022-02-25 09:16:43 +0700

    hello laravel-filepond

---
+++ learn-vuejs/examples/hello-filepond/resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+import bootstrap from "bootstrap";
+
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+@import "bootstrap/dist/css/bootstrap.min.css";
+</style>
