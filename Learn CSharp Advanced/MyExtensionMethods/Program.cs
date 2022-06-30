7f61b69a71221f0bd7d6e7bf1ed33423cd6639f7
8991nofear <8991nofear@gmail.com>
2022-06-30 22:14:49 +0700

    extension methods

---
+++ Learn CSharp Advanced/MyExtensionMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExtensionMethods
+{
+    class Program
+    {
+        /*
+         * 
+         * Extension method allow us to add new methods to an existing class without:
+         * +) Changing its source code or
+         * +) Create new class that inherits from it
+         * 
+         */
+        static void Main(string[] args)
+        {
+            var blogPost = "This is supposed to be a very long post ...";
+            var shortenedPost = blogPost.Shorten(13); // Shorten is available because of the namespace
+        }
+    }
+}
---
+++ Learn CSharp Advanced/MyExtensionMethods/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace MyExtensionMethods
+{
+    public static class StringExtensions // class must static
+    {
+        public static string Shorten(this string str, int noWords) // method must static
+        {
+            if (noWords <= 0)
+            {
+                throw new System.ArgumentOutOfRangeException("noWords must be greater than 0");
+            }
+
+            var words = str.Split(' ');
+            if (noWords <= words.Length)
+            {
+                return str;
+            }
+
+            return string.Join(" ", words.Take(noWords));
+        }
+    }
+}
