0c66dfe65896d19f282bfa8d5ecfcf79571dcc0f
8991nofear <8991nofear@gmail.com>
2022-07-07 23:05:37 +0700

    Exception Handling

---
+++ Learn CSharp Advanced/MyExceptionHandling/CustomException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyExceptionHandling
+{
+    /*
+     * Encapsulate low level exception 
+     * To make a user friendly message
+     */
+    class CustomException : Exception
+    {
+        public CustomException(string message, Exception innerException = default) : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ Learn CSharp Advanced/MyExceptionHandling/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExceptionHandling
+{
+    class Program
+    {
+        /*
+         * Exception in C# has Data and InnerException property
+         */
+        static void Main(string[] args)
+        {
+            // SYNTAX 1
+            StreamReader sr = default;
+            try
+            {
+                sr = new StreamReader(@"C:\doesnotexist.txt");
+                var content = sr.ReadToEnd();
+                Console.WriteLine(content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); // or Log
+            }
+            finally
+            {
+                if (sr != default)
+                {
+                    sr.Dispose();
+                }
+            }
+
+            // SYNTAX 2
+            try
+            {
+                using (var streamReader = new StreamReader(@"C:\doesnotexist.txt"))
+                {
+                    var content = streamReader.ReadToEnd();
+                    Console.WriteLine(content);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // PROBLEMS: trong catch ném tiếp exception:
+            // 1) sau catch có tiếp catch thì như thế nào?
+            // 2) sau catch chỉ có khối finally thì như thế nào?
+        }
+    }
+}
