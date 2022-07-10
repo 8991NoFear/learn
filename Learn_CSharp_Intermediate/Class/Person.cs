6ed97e82c0d4ab349bb6549d3565eda96f82f317
8991nofear <8991nofear@gmail.com>
2022-07-10 11:14:27 +0700

    Classes

---
+++ Learn_CSharp_Intermediate/Class/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace Class
+{
+    partial class Program
+    {
+        class Person
+        {
+            public string Name;
+
+            public Person()
+            {
+
+            }
+
+            public void Introduce(string to)
+            {
+                System.Console.WriteLine("Hi {0}, I am {1}", to, Name);
+            }
+
+            public static Person Parse(string name)
+            {
+                var p = new Person();
+                p.Name = name;
+
+                return p;
+            }
+        }
+    }
+}
---
+++ Learn_CSharp_Intermediate/Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    partial class Program
+    {
+        /*
+         * C# use PascalCase for name of class, property, method
+         * and camelCase for name of local variable, parameter
+         * 
+         * Why we use static member: To represent concept of singleton
+         */
+        static void Main(string[] args)
+        {
+            //var person = new Person();
+            var person = Person.Parse("Do Le");
+            person.Introduce("Le Binh");
+        }
+    }
+}
