cefb4a881120e520348ec8ce1b764c57021e9ad5
8991nofear <8991nofear@gmail.com>
2022-07-13 22:12:54 +0700

    GroupBy

---
+++ Learn_Linq/GroupBy/EmployeeRepo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace GroupBy
+{
+    class EmployeeRepo
+    {
+        public static List<Employee> GetListEmployees()
+        {
+            return new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "John", Gender = "Male", Department = "IT", Salary = 1122},
+                new Employee() { Id = 2, Name = "Mosh", Gender = "Male", Department = "HR", Salary = 1221},
+                new Employee() { Id = 3, Name = "Harry", Gender = "Female", Department = "IT", Salary = 1321},
+                new Employee() { Id = 4, Name = "Maguire", Gender = "Male", Department = "IT", Salary = 765},
+                new Employee() { Id = 5, Name = "Catherine", Gender = "Female", Department = "HR", Salary = 333},
+                new Employee() { Id = 6, Name = "Louis", Gender = "Female", Department = "SI", Salary = 559},
+                new Employee() { Id = 7, Name = "Louis", Gender = "Female", Department = "IT", Salary = 413},
+                new Employee() { Id = 8, Name = "Alice", Gender = "Female", Department = "HR", Salary = 507},
+                new Employee() { Id = 9, Name = "Bob", Gender = "Male", Department = "IT", Salary = 1579},
+                new Employee() { Id = 10, Name = "Cristiano", Gender = "Male", Department = "SI", Salary = 2575},
+                new Employee() { Id = 11, Name = "Ronaldo", Gender = "Male", Department = "SI", Salary = 1309},
+            };
+        }
+    }
+}
---
+++ Learn_Linq/GroupBy/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBy
+{
+    /*
+     * GroupBy creates and returns a sequence of IGrouping<K,V>
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GroupBy
+            GroupByMultipleKeys();
+        }
+
+        private static void GroupByMultipleKeys()
+        {
+            var eGroups = EmployeeRepo.GetListEmployees()
+                                    .GroupBy(e => new { e.Department, e.Gender })
+                                    .OrderBy(g => g.Key.Department)
+                                    .ThenBy(g => g.Key.Gender)
+                                    .Select(g => new
+                                    {
+                                        Department = g.Key.Department,
+                                        Gender = g.Key.Gender,
+                                        Employees = g.OrderBy(e => e.Name)
+                                    });
+
+            foreach (var eGroup in eGroups)
+            {
+                Console.WriteLine("Department: {0} - Gender: {1}", eGroup.Department, eGroup.Gender);
+                Console.WriteLine("----------------------");
+                foreach (var e in eGroup.Employees)
+                {
+                    Console.WriteLine("Name: {0}\tSalary: {1}", e.Name, e.Salary);
+                }
+                Console.WriteLine(); Console.WriteLine();
+            }
+        }
+
+        private static void GroupBy()
+        {
+            var eGroups = EmployeeRepo.GetListEmployees()
+                                    .GroupBy(e => e.Department)
+                                    .OrderBy(g => g.Key)
+                                    .Select(g => new
+                                    {
+                                        Key = g.Key,
+                                        Employees = g.OrderBy(e => e.Name),
+                                    });
+
+            foreach (var eGroup in eGroups)
+            {
+                Console.WriteLine("Department: {0} - Total salaries: {1}", eGroup.Key, eGroup.Employees.Sum(e => e.Salary));
+                Console.WriteLine("----------------------");
+
+                foreach (var e in eGroup.Employees)
+                {
+                    Console.WriteLine("Name: {0}\tSalary: {1}", e.Name, e.Salary);
+                }
+                Console.WriteLine(); Console.WriteLine();
+            }
+        }
+    }
+}
