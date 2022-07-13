62910d6234f6cd968db5af6e2740c1cec0cfe6cd
8991NoFear <57122131+8991NoFear@users.noreply.github.com>
2022-07-13 14:02:55 +0700

    Constructor

---
+++ Learn_CSharp_Intermediate/Constructor/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Constructor
+{
+    class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders; // = new List<Order>();
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        // unneccessary constructor
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+
+        // unneccessary constructor
+        public Customer(int id, string name)
+            : this(id)
+        {
+        }
+    }
+}
---
+++ Learn_CSharp_Intermediate/Constructor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructor
+{
+    /*
+     * What? Constructor is a method that is called when an instance of a class is created
+     * 
+     * Why? To put object in early state (to initialize fields)
+     * 
+     * How? "Exactly same as in Java":
+     *  +, Syntax ...
+     *  +, Default initialize for fields: number => 0, object reference (including string) => null, character => '\0'
+     *  +, Default constructor ...
+     *  +, Contructor overloading ...
+     *  
+     *  Best practices? Only create constructor when we really need to put object to wanted early state
+     */
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            // If we don't init Orders we will get NullReferenceException, but we should not init Orders here also
+            // Because Program class doesn't have responsibility for order, and if we ommit that and try do this
+            // When the application become bigger we have to remember all place where we init Orders => awful
+            //customer.Orders = new List<Order>();
+            customer.Orders.Add(new Order()); 
+        }
+    }
+}
