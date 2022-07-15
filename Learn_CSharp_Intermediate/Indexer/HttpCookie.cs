c754b4f098913583098199cccb428522b243961d
8991NoFear <57122131+8991NoFear@users.noreply.github.com>
2022-07-15 08:40:27 +0700

    Indexer

---
+++ Learn_CSharp_Intermediate/Indexer/HttpCookie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexer
+{
+    class HttpCookie
+    {
+        private Dictionary<string, string> _cookie;
+        private DateTime _expiry;
+
+        public HttpCookie(DateTime expiry)
+        {
+            _cookie = new Dictionary<string, string>();
+            _expiry = expiry;
+        }
+
+        public string this[string key]
+        {
+            get { return _cookie[key]; }
+            set { _cookie[key] = value; }
+        }
+    }
+}
---
+++ Learn_CSharp_Intermediate/Indexer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    /*
+     * What? Is a property but provide a way to access elements in class that represents as list of value
+     * Why? For convinence: eliminate SetItem() and GetItem() method
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie(DateTime.Now.AddDays(7));
+            cookie["name"] = "Binhld8";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
