f926e6aea48f304d27b202c7af95f2ce4cdd1c12
8991NoFear <57122131+8991NoFear@users.noreply.github.com>
2022-07-26 09:34:34 +0700

    Review ObjectInitializer

--- Learn_CSharp_Intermediate/ObjectInitializer/Program.cs
+++ Learn_CSharp_Intermediate/ObjectInitializer/Program.cs
@@ -7,15 +7,18 @@ using System.Threading.Tasks;
 namespace ObjectInitializer
 {
     /*
-     * What? Object initializer = default contructor + init fields
+     * What? Object initializer = contructor + init fields
      * Why? no need to write lots of constructor
      */
     class Program
     {
         static void Main(string[] args)
         {
+            //var customer = new Customer(10) { Name = "BinhDZ" };
+            //Console.WriteLine(customer.Id);
+
             // code below is wrong because Customer class doesn't have default constructor
-            //var customer = new Customer() { id = 1, name = "Binhld8" };
+            //var customer = new Customer() { Id = 1, Name = "Binhld8" };
         }
     }
 }
