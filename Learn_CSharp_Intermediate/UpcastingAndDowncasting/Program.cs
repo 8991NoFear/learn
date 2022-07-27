64b7725ad6dff973b1b8b7839323fa01f41260a4
8991NoFear <57122131+8991NoFear@users.noreply.github.com>
2022-07-27 08:33:11 +0700

    Upcasting and Downcasting

---
+++ Learn_CSharp_Intermediate/UpcastingAndDowncasting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpcastingAndDowncasting
+{
+    /*
+     * Base class variable and derived class variable reference to the same object in heap but they have different view
+     * => Because of that we have upcasting/downcasting concept
+     * 
+     * In the "inheritance hierarchy tree", start with the "actual type" of object
+     * +, Go up to the ancestor: upcasting (implicit)
+     * +, Go down to the descendant: downcasting (must explicit)
+     * 
+     * InvalidCastException may occur at runtime => use "as" keyword
+     * 
+     * The "is" keyword is identical as "instance of" keyword in Java
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
