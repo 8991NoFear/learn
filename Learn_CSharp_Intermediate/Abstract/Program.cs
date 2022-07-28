9c51b1a763c3c318be0f8474328c3b3996fda66c
8991NoFear <57122131+8991NoFear@users.noreply.github.com>
2022-07-28 09:07:01 +0700

    Abstract and Sealed

---
+++ Learn_CSharp_Intermediate/Abstract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    /*
+     * "abstract" keyword => indicate that class and method are missing implementation
+     * 
+     * Rules:
+     * +, if methods is abstract => class must be abstract too
+     * +, derived class must implement all abstract method
+     * +, abstract class can not instantiated
+     * 
+     * Why? to force other developer to folow your design
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>() { new Circle(), new Rectange() };
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
---
+++ Learn_CSharp_Intermediate/Sealed/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed
+{
+    /*
+     * "sealed" keyword
+     * +, applied to class => prevent derivation of class
+     * +, applied to method => prevent overriding of method
+     * 
+     * Why? Trust me, u don't need to use it :)
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
